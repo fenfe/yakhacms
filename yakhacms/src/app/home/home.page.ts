fe4a5cf115505c6085eca663e8950080d15902ad
fenfe <refdikutle@gmail.com>
2019-08-26 08:54:25 +0200

    converted this site into an SPA
    converted this site into an SPA

--- yakhacms/src/app/home/home.page.ts
+++ yakhacms/src/app/home/home.page.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  home: boolean = true;
+  messages: boolean = false;
+  homeowner: boolean = false;
+  homebuilder: boolean = false;
 
-  constructor() {}
+  constructor() {
+
+  }
+
+  homeFunc() {
+    this.home = true;
+    this.messages = false;
+    this.homeowner = false;
+    this.homebuilder = false;
+  }
+  messagesFunc() {
+    this.home = false;
+    this.messages = true;
+    this.homeowner = false;
+    this.homebuilder = false;
+  }
+  homeOwnerFunc() {
+    this.home = false;
+    this.messages = false;
+    this.homeowner = true;
+    this.homebuilder = false;
+
+  }
+  homebuilderFunc() {
+    this.home = false;
+    this.messages = false;
+    this.homeowner = false;
+    this.homebuilder = true;
+  }
 
 }
